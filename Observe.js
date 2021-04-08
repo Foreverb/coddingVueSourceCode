713b671813c5c457aae23fcd70afad8897b31da8
Foreverb <2740224530@qq.cm>
2021-04-08 23:10:25 +0800

    实现数据劫持,监听data数据

--- Observe.js
+++ Observe.js
@@ -1,5 +1,35 @@
 class Observe {
-  constructor() {
-    
-  }
+    constructor(data) {
+        //劫持监听data
+        this.observe(data);
+    }
+    observe(data) {
+        //只针对对象
+        if (data && typeof data === 'object') {
+            Object.keys(data).forEach(key => {
+                //劫持监听第一层data属性
+                this.observeReactnative(data, key, data[key])
+            })
+        }
+    }
+    observeReactnative(data, key, value) {
+        //递归遍历  劫持监听data所有属性
+        this.observe(data[key])
+            //通过 Object.definedProperty 方法劫持data属性
+        Object.defineProperty(data, key, {
+            configurable: false,
+            enumerable: true,
+            get: () => {
+                console.log('触发get方法');
+                return value;
+            },
+            set: (newVal) => {
+                console.log('触发set方法');
+                this.observe(newVal)
+                if (newVal !== value) {
+                    value = newVal;
+                }
+            }
+        })
+    }
 }
--- mVue.js
+++ mVue.js
@@ -1,131 +1,132 @@
 //编译指令
 const compileUtil = {
-  getVal(expr, vm) {
-    return [...expr.split('.')].reduce((returnVal, currenValue)=>{
-      return returnVal[currenValue]
-    }, vm.$data)
-  },
-  text( node, expr, vm ) {
-    let value;
-    if( expr.indexOf('{{') !== -1 ) {
-      value = expr.replace(/\{\{(.+?)\}\}/g, (...args) => {
-        return this.getVal(args[1], vm)
-      })
-    }else{
-      value = this.getVal(expr, vm);
+    getVal(expr, vm) {
+        return [...expr.split('.')].reduce((returnVal, currenValue) => {
+            return returnVal[currenValue]
+        }, vm.$data)
+    },
+    text(node, expr, vm) {
+        let value;
+        if (expr.indexOf('{{') !== -1) {
+            value = expr.replace(/\{\{(.+?)\}\}/g, (...args) => {
+                return this.getVal(args[1], vm)
+            })
+        } else {
+            value = this.getVal(expr, vm);
+        }
+        this.updater.textUpdater(node, value);
+    },
+    html(node, expr, vm) {
+        let value = this.getVal(expr, vm);
+        this.updater.htmlUpdater(node, value);
+    },
+    model(node, expr, vm) {
+        let value = this.getVal(expr, vm);
+        this.updater.modelUpdater(node, value);
+    },
+    on(node, expr, vm, eventName) {
+        const func = vm.$options.methods && vm.$options.methods[expr];
+        node.addEventListener(eventName, func.bind(vm), false)
+    },
+    //数据驱动视图，更新视图
+    updater: {
+        textUpdater(node, val) {
+            node.textContent = val;
+        },
+        htmlUpdater(node, val) {
+            node.innerHTML = val;
+        },
+        modelUpdater(node, val) {
+            node.value = val;
+        },
     }
-    this.updater.textUpdater(node, value);
-  },
-  html( node, expr, vm ) {
-    let value = this.getVal(expr, vm);
-    this.updater.htmlUpdater(node, value);
-  },
-  model( node, expr, vm ) {
-    let value = this.getVal(expr, vm);
-    this.updater.modelUpdater(node, value);
-  },
-  on( node, expr, vm, eventName) {
-    const func = vm.$options.methods && vm.$options.methods[expr];
-    node.addEventListener(eventName, func.bind(vm), false)
-  },
-  //数据驱动视图，更新视图
-  updater: {
-    textUpdater(node, val){
-      node.textContent = val;
-    },
-    htmlUpdater(node, val){
-      node.innerHTML = val;
-    },
-    modelUpdater(node, val){
-      node.value = val;
-    },
-  }
 }
 class Compile {
-  constructor(el, vm) {
-    this.vm = vm;
-    this.el = this.isElementType(el) ? el : document.querySelector(el);
-    //1. 将所有节点插入到文档碎片中（使用文档碎片可以减少重绘重排，提升性能）
-    const fragment = this.nodeFragment(this.el);
-    //2. 指令解析
-    this.compile(fragment)
+    constructor(el, vm) {
+        this.vm = vm;
+        this.el = this.isElementType(el) ? el : document.querySelector(el);
+        //1. 将所有节点插入到文档碎片中（使用文档碎片可以减少重绘重排，提升性能）
+        const fragment = this.nodeFragment(this.el);
+        //2. 指令解析
+        this.compile(fragment)
 
-    //3. 将文档碎片挂载到根元素里面
-    this.el.appendChild(fragment)
-  }
-  compile(fragment) {
-    let childList = fragment.childNodes;
-    [...childList].forEach(child=>{
-      if( this.isElementType(child) ) {
-        //编译元素节点
-        this.compileElement(child);
-      }else{
-        //编译文本节点
-        this.compileText(child);
-      }
-      if( child.childNodes && child.childNodes.length ) {
-        this.compile(child)
-      }
-    })
-  }
-  //compileElement编译元素节点
-  compileElement(node) {
-    const childAttr = node.attributes;
-    [...childAttr].forEach(attr=>{
-      const {name, value} = attr;
-      if( this.isDirective(name) ) {
-        const [,directive] = name.split('-');
-        const [directiveName, eventName] = directive.split(':');
-        //更新视图，数据驱动视图
-        compileUtil[directiveName](node,value,this.vm,eventName);
-        //移除行间指令
-        node.removeAttribute(`v-${directiveName}`);
-        eventName && node.removeAttribute(`v-${directiveName}:${eventName}`)
-      } else if( this.isEventName(name) ) {
-        const [, eventName] = name.split('@');
-        compileUtil['on'](node,value,this.vm,eventName)
-      }
-    })
-  }
-  //compileNode编译文本节点
-  compileText(node) { 
-    // {{}} 
-    const content = node.textContent;
-    if( /\{\{.+?\}\}/.test(content) ) {
-      compileUtil['text'](node, content, this.vm)
+        //3. 将文档碎片挂载到根元素里面
+        this.el.appendChild(fragment)
     }
-  }
-  //判断是否是通过@绑定事件
-  isEventName(attr) {
-    return attr.startsWith('@');
-  }
-  //判断是否为指令
-  isDirective(attr) {
-    return attr.startsWith('v-');
-  }
-  //创建文档碎片s
-  nodeFragment(el) {
-    const f = document.createDocumentFragment();
-    let firstChild;
-    while (firstChild = el.firstChild) {
-      f.appendChild(firstChild);
+    compile(fragment) {
+            let childList = fragment.childNodes;
+            [...childList].forEach(child => {
+                if (this.isElementType(child)) {
+                    //编译元素节点
+                    this.compileElement(child);
+                } else {
+                    //编译文本节点
+                    this.compileText(child);
+                }
+                if (child.childNodes && child.childNodes.length) {
+                    this.compile(child)
+                }
+            })
+        }
+        //compileElement编译元素节点
+    compileElement(node) {
+            const childAttr = node.attributes;
+            [...childAttr].forEach(attr => {
+                const { name, value } = attr;
+                if (this.isDirective(name)) {
+                    const [, directive] = name.split('-');
+                    const [directiveName, eventName] = directive.split(':');
+                    //更新视图，数据驱动视图
+                    compileUtil[directiveName](node, value, this.vm, eventName);
+                    //移除行间指令
+                    node.removeAttribute(`v-${directiveName}`);
+                    eventName && node.removeAttribute(`v-${directiveName}:${eventName}`)
+                } else if (this.isEventName(name)) {
+                    const [, eventName] = name.split('@');
+                    compileUtil['on'](node, value, this.vm, eventName)
+                }
+            })
+        }
+        //compileNode编译文本节点
+    compileText(node) {
+            // {{}} 
+            const content = node.textContent;
+            if (/\{\{.+?\}\}/.test(content)) {
+                compileUtil['text'](node, content, this.vm)
+            }
+        }
+        //判断是否是通过@绑定事件
+    isEventName(attr) {
+            return attr.startsWith('@');
+        }
+        //判断是否为指令
+    isDirective(attr) {
+            return attr.startsWith('v-');
+        }
+        //创建文档碎片s
+    nodeFragment(el) {
+            const f = document.createDocumentFragment();
+            let firstChild;
+            while (firstChild = el.firstChild) {
+                f.appendChild(firstChild);
+            }
+            return f;
+        }
+        //判断元素是否是元素节点
+    isElementType(node) {
+        return node.nodeType === 1
     }
-    return f;
-  }
-  //判断元素是否是元素节点
-  isElementType(node) {
-    return node.nodeType === 1
-  }
 }
 class mVue {
-  constructor(options){
-    this.$el = options.el;
-    this.$data = options.data;
-    this.$options = options;
-    //1. 实现一个指令解析器
-    if( this.$el ) {
-      new Compile(this.$el, this)
+    constructor(options) {
+        this.$el = options.el;
+        this.$data = options.data;
+        this.$options = options;
+        //1. 实现一个指令解析器
+        if (this.$el) {
+            new Compile(this.$el, this)
+        }
+        //2. 实现一个数据监听器
+        new Observe(this.$data)
     }
-    //2. 实现一个数据监听器
-  }
 }
